3b637e2a8163865754a48d06e21d3bc5fe74d34a
Reshma <reshma.a@Technovert.com>
2022-05-11 20:10:39 +0530

    change1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import './index.css';
+import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
+import LogIn from "./pages/login/login.jsx"
+import SignUp from "./pages/signup/signup.jsx"
+import Choose from "./pages/choose/choose.jsx"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<LogIn/>}></Route>
+        <Route exact path='/signup' element={<SignUp/>}></Route>
+        <Route exact path='/selection' element={<Choose/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
